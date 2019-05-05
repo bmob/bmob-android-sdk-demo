81332bc3cfe6460bab40f23fc628ab68c49b6b85
chaozhouzhang <13760289294@139.com>
2019-05-05 10:44:45 +0800

    更新到SDK v3.7.3-rc2 兼容数据监听功能到android9.0

--- DataSdkDemo/app/src/main/java/cn/bmob/sdkdemo/activity/realtime/RealTimeDataActivity.java
+++ DataSdkDemo/app/src/main/java/cn/bmob/sdkdemo/activity/realtime/RealTimeDataActivity.java
@@ -1,9 +1,9 @@
 package cn.bmob.sdkdemo.activity.realtime;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-
-import com.google.gson.Gson;
+import android.widget.Toast;
 
 import org.json.JSONObject;
 
@@ -17,7 +17,9 @@ import cn.bmob.sdkdemo.bean.Chat;
 import cn.bmob.v3.BmobRealTimeData;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.SaveListener;
-import cn.bmob.v3.listener.ValueEventListener;
+import cn.bmob.v3.realtime.Client;
+import cn.bmob.v3.realtime.RealTimeDataListener;
+import cn.bmob.v3.realtime.RealTimeDataManager;
 
 /**
  * 数据监听是付费功能，请确保您appkey对应的应用已开通数据监听功能
@@ -38,42 +40,74 @@ public class RealTimeDataActivity extends AppCompatActivity  {
 
     private void startBmobRealTimeData() {
 
-        final BmobRealTimeData bmobRealTimeData = new BmobRealTimeData();
-        bmobRealTimeData.start(new ValueEventListener() {
+        //TODO 旧版数据监听，不兼容9.0
+//        final BmobRealTimeData bmobRealTimeData = new BmobRealTimeData();
+//        bmobRealTimeData.start(new ValueEventListener() {
+//            @Override
+//            public void onConnectCompleted(Exception e) {
+//                if (e == null) {
+//                    mTvInfo.setText("连接情况：" + (bmobRealTimeData.isConnected() ? "已连接" : "未连接") + "\n");
+//                    if (bmobRealTimeData.isConnected()) {
+//                        //TODO 如果已连接，设置监听动作为：监听Chat表的更新
+//                        bmobRealTimeData.subTableUpdate("Chat");
+//                    }
+//                } else {
+//                    mTvInfo.setText("连接出错：" + e.getMessage() + "\n");
+//                }
+//            }
+//
+//            @Override
+//            public void onDataChange(JSONObject jsonObject) {
+//                /**
+//                 * {"nameValuePairs":{"appKey":"f25fe6dad5bca9d0bb090072ea1e3c65","tableName":"Chat","objectId":"","action":"updateTable",
+//                 * "data":{"nameValuePairs":{"content":"更新Chat表测试+1537324483401","createdAt":"2018-09-19 10:35:00","name":"RDT","objectId":"d5fffe82e9","updatedAt":"2018-09-19 10:35:00"}}}}
+//                 */
+//                Gson gson = new Gson();
+//                String action = jsonObject.optString("action");
+//                String jsonString = gson.toJson(jsonObject);
+//                mTvInfo.setText("更新返回内容是：" + jsonString + "\n");
+//                mTvInfo.setText("当前更新动作是：" + action + "\n");
+//                if (action.equals(BmobRealTimeData.ACTION_UPDATETABLE)) {
+//                    //TODO 如果监听表更新
+//                    JSONObject data = jsonObject.optJSONObject("data");
+//                    mTvInfo.setText("name：" + data.optString("name") + "\n");
+//                    mTvInfo.setText("content：" + data.optString("content") + "\n");
+//                    mTvInfo.setText("监听到更新：" + data.optString("name") + "-" + data.optString("content") + "\n");
+//                }
+//            }
+//        });
+
+
+        //TODO 新版数据监听，兼容9.0
+        RealTimeDataManager.getInstance().start(new RealTimeDataListener() {
             @Override
-            public void onConnectCompleted(Exception e) {
+            public void onConnectCompleted(Client client, Exception e) {
                 if (e == null) {
-                    mTvInfo.setText("连接情况：" + (bmobRealTimeData.isConnected() ? "已连接" : "未连接") + "\n");
-                    if (bmobRealTimeData.isConnected()) {
-                        //TODO 如果已连接，设置监听动作为：监听Chat表的更新
-                        bmobRealTimeData.subTableUpdate("Chat");
-                    }
+                    System.out.println("数据监听：已连接");
+                    client.subTableUpdate("Chat");
+                    Toast.makeText(RealTimeDataActivity.this, "已连接", Toast.LENGTH_SHORT).show();
                 } else {
-                    mTvInfo.setText("连接出错：" + e.getMessage() + "\n");
+                    Toast.makeText(RealTimeDataActivity.this, "连接出错：" + e.getMessage() , Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
-            public void onDataChange(JSONObject jsonObject) {
+            public void onDataChange(Client client, JSONObject jsonObject) {
                 /**
                  * {"nameValuePairs":{"appKey":"f25fe6dad5bca9d0bb090072ea1e3c65","tableName":"Chat","objectId":"","action":"updateTable",
                  * "data":{"nameValuePairs":{"content":"更新Chat表测试+1537324483401","createdAt":"2018-09-19 10:35:00","name":"RDT","objectId":"d5fffe82e9","updatedAt":"2018-09-19 10:35:00"}}}}
                  */
-                Gson gson = new Gson();
+                Log.e("onDataChange",jsonObject.toString());
+                //更新动作
                 String action = jsonObject.optString("action");
-                String jsonString = gson.toJson(jsonObject);
-                mTvInfo.setText("更新返回内容是：" + jsonString + "\n");
-                mTvInfo.setText("当前更新动作是：" + action + "\n");
                 if (action.equals(BmobRealTimeData.ACTION_UPDATETABLE)) {
                     //TODO 如果监听表更新
+                    //更新内容
                     JSONObject data = jsonObject.optJSONObject("data");
-                    mTvInfo.setText("name：" + data.optString("name") + "\n");
-                    mTvInfo.setText("content：" + data.optString("content") + "\n");
-                    mTvInfo.setText("监听到更新：" + data.optString("name") + "-" + data.optString("content") + "\n");
+                    Toast.makeText(RealTimeDataActivity.this, "监听到更新：" + data.optString("name") + "-" + data.optString("content"), Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
     }
 
     @OnClick({R.id.btn_connect_listen, R.id.btn_update_chat})
